fdc931392a39a4925b2aaf781e2b1fb08462e8d4
myrat-stack <2.34433402e+08+myrat-stack@users.noreply.github.com>
2026-08-20 15:58:38 +0000

    Fix subscription endpoint reverse proxy

--- install.sh
+++ install.sh
@@ -126,6 +126,16 @@ server {
         proxy_set_header X-Real-IP \$remote_addr;
     }
 
+    location /sub/ {
+        proxy_pass http://127.0.0.1:${XRAY_PORT}/sub/;
+        proxy_http_version 1.1;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+        proxy_buffering off;
+    }
+
     location /dashboard/ {
         proxy_pass http://127.0.0.1:${XRAY_PORT}/dashboard/;
         proxy_http_version 1.1;
